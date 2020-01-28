53bd13fe27b2a4e72c8d3d03df3aee88862d4391
Pyrofab <redstoneinfire@gmail.com>
2020-01-28 19:46:56 +0100

    Add license headers

--- src/main/java/io/github/ladysnake/impersonate/impl/ImpersonateGamerules.java
+++ src/main/java/io/github/ladysnake/impersonate/impl/ImpersonateGamerules.java
@@ -1,3 +1,20 @@
+/*
+ * Impersonate
+ * Copyright (C) 2020 Ladysnake
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses>.
+ */
 package io.github.ladysnake.impersonate.impl;
 
 import com.mojang.brigadier.arguments.BoolArgumentType;
@@ -22,11 +39,11 @@ public final class ImpersonateGamerules {
         register("impersonate:opRevealImpersonations", createBooleanRule(true, (server, rule) -> {}));
 
     @SuppressWarnings("unchecked")
-    public static GameRules.RuleType<GameRules.BooleanRule> createBooleanRule(boolean initialValue, BiConsumer<MinecraftServer, GameRules.BooleanRule> changeCallback) {
+    private static GameRules.RuleType<GameRules.BooleanRule> createBooleanRule(boolean initialValue, BiConsumer<MinecraftServer, GameRules.BooleanRule> changeCallback) {
         return RuleTypeAccessor.invokeNew(BoolArgumentType::bool, type -> new GameRules.BooleanRule((GameRules.RuleType<GameRules.BooleanRule>) type, initialValue), changeCallback);
     }
 
-    public static <T extends GameRules.Rule<T>> GameRules.RuleKey<T> register(String name, GameRules.RuleType<T> type) {
+    private static <T extends GameRules.Rule<T>> GameRules.RuleKey<T> register(String name, GameRules.RuleType<T> type) {
         return GameRulesAccessor.invokeRegister(name, type);
     }
 

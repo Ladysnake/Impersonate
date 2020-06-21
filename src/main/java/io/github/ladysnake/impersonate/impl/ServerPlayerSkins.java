d75b6fd42f7a8dc837f1cfd8ccd41cb4d698c41c
Pyrofab <redstoneinfire@gmail.com>
2020-06-22 00:25:52 +0200

    Update license headers

--- src/main/java/io/github/ladysnake/impersonate/impl/ServerPlayerSkins.java
+++ src/main/java/io/github/ladysnake/impersonate/impl/ServerPlayerSkins.java
@@ -1,11 +1,11 @@
 /*
- * FabricTailor
- * Copyright (C) 2020 samo_lego
+ * Impersonate
+ * Copyright (C) 2020 Ladysnake
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version ${gplVersion} of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of

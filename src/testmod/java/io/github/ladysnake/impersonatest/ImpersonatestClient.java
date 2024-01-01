67bca7bcd75bbae20961bebc2d688e9f31b560e9
Pyrofab <redstoneinfire@gmail.com>
2024-01-01 23:55:01 +0100

    Add debug helper to login in dev env

---
+++ src/testmod/java/io/github/ladysnake/impersonatest/ImpersonatestClient.java
@@ -0,0 +1,35 @@
+/*
+ * Impersonate
+ * Copyright (C) 2020-2024 Ladysnake
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses>.
+ */
+package io.github.ladysnake.impersonatest;
+
+import io.github.ladysnake.impersonate.Impersonate;
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
+
+public final class ImpersonatestClient implements ClientModInitializer {
+    private static final boolean debugCredentials = Boolean.getBoolean("impersonate.debug.printCredentials");
+
+    @Override
+    public void onInitializeClient() {
+        if (debugCredentials) {
+            // Here to help login within a dev environment - be very careful when running this code at home
+            Impersonate.LOGGER.info("ACCESS_TOKEN " + MinecraftClient.getInstance().getSession().getAccessToken());
+            Impersonate.LOGGER.info("UUID " + MinecraftClient.getInstance().getSession().getUuidOrNull());
+        }
+    }
+}

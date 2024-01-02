95b2fdf34c0146c705d7d764791304ff926046a4
Pyrofab <redstoneinfire@gmail.com>
2024-01-02 01:30:21 +0100

    Update license headers

--- src/main/java/io/github/ladysnake/impersonate/impl/RecipientAwareText.java
+++ src/main/java/io/github/ladysnake/impersonate/impl/RecipientAwareText.java
@@ -1,6 +1,6 @@
 /*
  * Impersonate
- * Copyright (C) 2020-2023 Ladysnake
+ * Copyright (C) 2020-2024 Ladysnake
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
